4fd37610409cf0d51a41b85db64a101e4c04e6db
PedroScheeffer <PEDRO.SCHEEFFER@correo.ucu.edu.uy>
2024-05-20 20:13:14 -0300

    input

---
+++ src/components/MyInput/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyInput = () => {
+    return (
+        <div>
+            <h1>Component Name</h1>
+            <p>Component Content</p>
+        </div>
+    );
+};
+
+export default MyInput;
