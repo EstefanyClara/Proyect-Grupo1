b7337adc4ade436d60e4bc55b96849c28a023676
PedroScheeffer <pedro.scheeffer@correo.ucu.edu.uy>
2024-06-12 16:37:40 -0300

    adding plataforms and fixin imports

--- src/Pages/GameDetails/gameDetails.jsx
+++ src/Pages/GameDetails/gameDetails.jsx
@@ -1,33 +1,49 @@
 import React from 'react';
 import { useState, useEffect, useContext } from 'react';
-import NavBarSimple from '../../components/NavBarSimple/NavBarSimple';
-
+import { getGame } from '../../api';
 import { ThemeContext } from "../../components/context/context";
-import {getGame} from '../../api';
+import NavBarSimple from '../../components/NavBarSimple/NavBarSimple';
+import Skeleton from '../../components/MySkeleton/MySkeleton';
+import Plataforms from '../../components/Plataforms/Plataforms';
 const GameDetails = (gameId) => {
     const [gameData, setGameData] = useState(null);
     const themeSettings = useContext(ThemeContext);
+    const [isLoading, setisLoading] = useState(true);
 
     useEffect(() => {
         const fetchGameData = async () => {
+            setisLoading(true);
             try {
                 const game = await getGame(3498);
                 setGameData(game);
             } catch (error) {
                 console.error('Error fetching game data:', error);
             }
+            setisLoading(false);
         };
 
         fetchGameData();
     }, []);
 
-    return (
-        <div className={'game-details-' + themeSettings.mode }>
+
+    if (isLoading) {
+        return(
+        <div className={'game-details-' + themeSettings.mode}>
             <NavBarSimple />
-            <h1>{gameData.name}</h1>
-            
+            <Skeleton />;
         </div>
-    );
+        );
+    } else {
+        return (
+            <div className={'game-details-' + themeSettings.mode}>
+                <NavBarSimple />
+                <Plataforms plataforms={gameData?.parent_platforms}/>
+                
+                <card>{gameData?.name}</card>
+            </div>
+        );
+    }
+
 };
 
 export default GameDetails;
