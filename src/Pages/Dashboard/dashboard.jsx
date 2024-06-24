2b1131850af70e8c7017ca278bdeb4095d0c85ef
PedroScheeffer <pedro.scheeffer@correo.ucu.edu.uy>
2024-06-24 18:53:51 -0300

    css

--- src/Pages/Dashboard/dashboard.jsx
+++ src/Pages/Dashboard/dashboard.jsx
@@ -1,17 +1,21 @@
 import { Navbar } from "../../components/Navbar/navbar";
 import "./dashboard.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {LogoutModal} from "../../components/LogoutModal/LogoutModal";
+import {SideBar} from "../../components/SideBar/SideBar";
+import { useState } from 'react';
 
 
 export const DashBoard = () => {
+    const [category, setCategory] = useState('');
+
 
     return (
         <div>
             <Navbar/>
             <h1> New And Trending </h1>
             <p> Based on player couts and release date</p>
-            <LogoutModal/>
+            <SideBar onChangeCategory={setCategory}/>
+            
         </div>
     );
 }; export default DashBoard;
