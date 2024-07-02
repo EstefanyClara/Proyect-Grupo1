ffe4c0b2f16bb0d22fc607347e4b8b511f9e4bc1
PedroScheeffer <pedro.scheeffer@correo.ucu.edu.uy>
2024-07-02 20:24:28 -0300

    card

--- src/Components/GameCard/GameCard.jsx
+++ src/Components/GameCard/GameCard.jsx
@@ -1,30 +1,32 @@
 import React, { useState, useEffect, useContext } from "react";
-import {ThemeContext} from "../Context/Context";
+import { ThemeContext } from "../Context/Context";
 import MySkeleton from "../MySkeleton/MySkeleton";
 import Plataforms from "../Plataforms/Plataforms";
+import "./GameCard.css";
 const GameCard = ({ gameData, listNumber, isLoading }) => {
-  
   const themeSettings = useContext(ThemeContext);
   if (!gameData) {
     return <div>Loading...</div>;
   }
 
   return (
-    <div className="container">
-      <div
-        className={`card-${themeSettings.mode}`}
-        style={{
-          background: `linear-gradient(rgba(0, 0, 255, 0), rgba(0, 0, 0, 1)),
-            url(${gameData?.background_image}) no-repeat`,
-        }}
-      >
-        {isLoading ? (
-          <MySkeleton />
-        ) : (
-          <div className="loading-content">
-            <h1 className="Title">{gameData?.name}</h1>
-            <Plataforms plataforms={gameData?.parent_platforms}/>
-            <p>#{listNumber}</p>
+    <div className={`card card-${themeSettings.mode}`}>
+      {isLoading ? (
+        <MySkeleton />
+      ) : (
+        <>
+          <div
+            className="background-image"
+            style={{
+              backgroundImage: `url(${gameData?.background_image})`,
+            }}
+          ></div>
+          <div className="details">
+            <div>
+              <h1 className="title">{gameData?.name}</h1>
+              <p className="number-info">#{listNumber}</p>
+            </div>
+
             <div className="details-block">
               <p>Realase Date: {gameData?.released}</p>
               <p>
@@ -34,9 +36,10 @@ const GameCard = ({ gameData, listNumber, isLoading }) => {
                 })}
               </p>
             </div>
+            <Plataforms plataforms={gameData?.parent_platforms} />
           </div>
-        )}
-      </div>
+        </>
+      )}
     </div>
   );
 };
