546094798030d6b0964d0b0fe276c28d829d758b
PedroScheeffer <pedro.scheeffer@correo.ucu.edu.uy>
2024-07-03 21:14:36 -0300

    removing depracated

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { useState, useEffect } from 'react'
 import { ThemeContext } from "./Components/Context/Context";
 import './App.css'
 // Pages
-import GameDetails from "./Components/GameDetails/GameDetails";
 import DashBoard from "./Pages/Dashboard/DashBoard";
 import Home from "./Pages/Home/Home";
 //importando los modulos de firebase
